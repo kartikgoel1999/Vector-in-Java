47b3d6bcafe960a86508334ae5cd22caf96616bb
kartikgoel1999 <52077666+kartikgoel1999@users.noreply.github.com>
2020-01-16 19:44:50 +0530

    Add files via upload

---
+++ VectorDemo.java
@@ -0,0 +1,18 @@
+import java.util.*;
+class VectorDemo 
+{
+	public static void main(String[] args) 
+	{
+		Vector<String> vec=new Vector<String>();
+		
+		vec.add("First");
+		vec.add("2");
+        vec.add("First");
+		vec.add("2");
+		vec.add("First");
+		vec.add("2");
+		vec.add("First");
+	    System.out.println(vec);
+		
+	}
+}
